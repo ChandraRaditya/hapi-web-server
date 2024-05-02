f276227d4e410d8832d9e754af6f066172ac66cb
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-05-02 17:48:07 +0700

    feat: add payload and params

--- routes.js
+++ routes.js
@@ -7,10 +7,59 @@ const routes = [
     },
   },
   {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
     method: "GET",
     path: "/about",
     handler: (request, h) => {
       return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}", // memberikan efek null safety
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params; // untuk path params
+      const { lang } = request.query; // untuk query params
+      if (lang === "id") {
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}`;
+    },
+  },
+  {
+    method: "GET",
+    path: "/coba/{test*}", // dengan * ini akan memberikan efek bisa disi parameter sebanyak yang kita mau, sepertu /chandra/ari
+    handler: (request, h) => {
+      const { test = "stranger" } = request.params;
+      return `Hello, ${test}`;
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+  {
+    method: "POST",
+    path: "/login",
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}`;
     },
   },
 ];
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Hapi = require("@hapi/hapi");
+const Routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
@@ -6,8 +7,10 @@ const init = async () => {
     host: "localhost",
   });
 
+  server.route(Routes);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-init()
+init();
